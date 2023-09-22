83370ef0a90cc14a0e8bbd8177d17ee5a4dfc080
Debadyuti <debadyuti.karmakar@gmail.com>
2023-09-22 11:20:28 +0530

    Fevourites

--- src/components/Fevourites.jsx
+++ src/components/Fevourites.jsx
@@ -1,10 +1,29 @@
+import Pagination from "./Pagination";
 
 function Frvourites() {
   return (
     <div>
-      <h1>Fevourites</h1>
+      <div className="flex mt-4 p-2 justify-center gap-5">
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-xl">
+          All Genres
+        </button>
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-xl">
+          All Genres
+        </button>
+      </div>
+      <div className="flex mt-4 p-2 justify-center gap-5">
+        <input type="text" placeholder="search"
+        className="border-2 p-2 rounded-xl text-center"
+        ></input>
+        <input type="number" value={1}
+        className="border-2 p-2 rounded-xl text-center"
+        ></input>
+      </div>
+      <h2>Searching</h2>
+      <h2>Data Table</h2>
+      <Pagination></Pagination>
     </div>
-  )
+  );
 }
 
-export default Frvourites
+export default Frvourites;
